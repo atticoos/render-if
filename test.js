f7c3b7a0f3b2a189e1aaef705dec2fc83c54bef0
Atticus White <contact@atticuswhite.com>
2016-02-08 22:13:13 -0500

    added some spy tests for @lukewestby's thunk added in #2

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 'use strict';
 
-import {expect} from 'chai';
+import chai, {expect} from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 import renderIf from './renderIf';
+
+chai.use(sinonChai);
 
 describe('renderIf', () => {
   it('should return a function', () => {
@@ -21,6 +25,16 @@ describe('renderIf', () => {
     });
     it('should not return the result of the thunk when the predicate fails', () => {
       expect(renderIf(false)(() => 'foobar')).to.be.eql(null);
+    });
+    it ('should call the thunk when the predicate passes', () => {
+      var spy = sinon.spy();
+      renderIf(true)(spy);
+      expect(spy).to.have.been.called;
+    });
+    it ('should not call the thunk when the predicate fails', () => {
+      var spy = sinon.spy();
+      renderIf(false)(spy);
+      expect(spy).not.to.have.been.called;
     });
   });
 });
