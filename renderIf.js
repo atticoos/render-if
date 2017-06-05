c7f5fe1ef0c31a2d1c4813265456e046d69d6e6a
Atticus White <contact@atticuswhite.com>
2017-06-05 19:28:41 -0400

    conditional case support

--- renderIf.js
+++ renderIf.js
@@ -2,5 +2,54 @@
 
 const isFunction = input => typeof input === 'function';
 
-export default predicate => elemOrThunk =>
-  predicate ? (isFunction(elemOrThunk) ? elemOrThunk() : elemOrThunk) : null;
+function renderIf (predicate) {
+  return elemOrThunk => predicate ? (isFunction(elemOrThunk) ? elemOrThunk() : elemOrThunk) : null;
+}
+
+renderIf.if = function multiStatement (initialCondition) {
+  return renderIf.multi().if(initialCondition);
+}
+
+renderIf.multi = function () {
+  const cases = [];
+
+  function createCondition (priority) {
+    return function conditionCase (condition) {
+      return function conditionComponent (elemOrThunk) {
+        cases.push({
+          priority,
+          condition,
+          elemOrThunk
+        });
+
+        return api;
+      }
+    }
+  }
+
+  function evaluate() {
+    let sortedCases = cases.sort((a, b) => a.priority > b.priority);
+
+    for (let i = 0; i < sortedCases.length; i++) {
+      if (sortedCases[i].condition) {
+        if (isFunction(sortedCases[i].elemOrThunk)) {
+          return sortedCases[i].elemOrThunk();
+        }
+        return sortedCases[i].elemOrThunk;
+      }
+    }
+
+    return null;
+  }
+
+  const api = {
+    if: createCondition(0),
+    elseIf: createCondition(1),
+    else: createCondition(2),
+    evaluate
+  };
+
+  return api;
+}
+
+export default renderIf;
